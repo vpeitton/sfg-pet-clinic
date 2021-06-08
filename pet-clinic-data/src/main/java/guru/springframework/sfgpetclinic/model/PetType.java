a759e1b6b04704497e12a58db2b6f94b1b939644
vpeitton <vpeitton@gmail.com>
2021-06-08 17:36:03 -0300

    commit!

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,35 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
-import lombok.*;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-/**
- * Created by jt on 7/13/18.
- */
-
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "types")
-public class PetType extends BaseEntity {
-
-    @Builder
-    public PetType(Long id, String name) {
-        super(id);
-        this.name = name;
-    }
-
-    @Column(name = "name")
+public class PetType {
     private String name;
 
-
-    @Override
-    public String toString() {
+    public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
