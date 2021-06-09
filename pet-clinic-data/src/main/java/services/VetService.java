a907c02ff37497dd50e900e1fc5d07c5f04e31f8
vpeitton <vpeitton@gmail.com>
2021-06-09 17:41:56 -0300

    commit!

--- pet-clinic-data/src/main/java/services/VetService.java
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -2,10 +2,7 @@ package services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
+
 }
