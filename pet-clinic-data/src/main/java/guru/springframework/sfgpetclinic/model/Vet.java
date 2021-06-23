03bf351ec29472fb2083e93cf72d07783cbe1fc5
vpeitton <vpeitton@gmail.com>
2021-06-23 13:06:25 -0300

    commit!

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Vet extends Person {
+import java.util.Set;
 
+public class Vet extends Person {
+    private Set<Speciality> specialties;
+
+    public Set<Speciality> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Speciality> specialties) {
+        this.specialties = specialties;
+    }
 }
