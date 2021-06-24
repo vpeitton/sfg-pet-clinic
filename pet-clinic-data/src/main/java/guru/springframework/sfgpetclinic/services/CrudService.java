287537e4ba2d39c1af9489fdcc7a304bd70799c1
vpeitton <vpeitton@gmail.com>
2021-06-24 14:44:24 -0300

    commit! Closes #4

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -2,12 +2,12 @@ package guru.springframework.sfgpetclinic.services;
 
 import java.util.Set;
 
-public interface CrudService<T, ID> {
+public interface CrudService<T, Long> {
 
     Set<T> findAll();
-    T findById(ID id);
+    T findById(Long id);
     T save(T object);
     void delete(T object);
-    void deleteById(ID id);
+    void deleteById(Long id);
 
 }
