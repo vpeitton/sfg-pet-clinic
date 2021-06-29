d8af87231ce2d0c4c9500748335fff246ed9788b
vpeitton <vpeitton@gmail.com>
2021-06-29 16:37:03 -0300

    commit!

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/NamedEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/NamedEntity.java
@@ -1,12 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class NamedEntity extends BaseEntity{
@@ -17,6 +21,11 @@ public class NamedEntity extends BaseEntity{
     @Column(name = "last_name")
     private String lastName;
 
+    public NamedEntity(Long id, String lastName, String firstName) {
+        super(id);
+        this.lastName = lastName;
+        this.firstName=firstName;
+    }
 
     @Override
     public String toString() {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,8 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person extends NamedEntity{
 
+    public Person(Long id, String firstName, String lastName) {
+        super(id,lastName,firstName);
+    }
 }
